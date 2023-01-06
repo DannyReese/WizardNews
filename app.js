36ac6912e5b190f721605e17c6260f2d419cf486
DannyReese <113713130+DannyReese@users.noreply.github.com>
2023-01-05 21:00:04 -0500

    commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const morgan = require("morgan")
+ const morgan = require("morgan")
 const app = express();
 
 
@@ -8,11 +8,6 @@ app.get("/", (req, res) => res.send('wizard newws'));
 const PORT = 1337;
 
 app.listen(PORT, () => {
-<<<<<<< HEAD
-
-=======
->>>>>>> 80b6796ebe3b793590a7a056abcd9dd4ba74bfd4
-  console.log("hey hey");
 });
 
 app.use(morgan('dev'));
