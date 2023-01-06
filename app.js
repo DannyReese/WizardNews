80b6796ebe3b793590a7a056abcd9dd4ba74bfd4
Wubbiez <108950491+Wubbiez@users.noreply.github.com>
2023-01-05 20:32:52 -0500

    zach changes

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const morgan = require("morgan")
 const app = express();
+
 
 app.get("/", (req, res) => res.send('wizard newws'));
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
-  // app.get("/", (req, res) => res.send("Hello"));
   console.log("hey hey");
 });
+
+app.use(morgan('dev'));
