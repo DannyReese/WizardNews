7bad8c4dca9212aad9e28f77c1c545c7e93bd212
DannyReese <113713130+DannyReese@users.noreply.github.com>
2023-01-06 18:01:32 -0500

    cleaned upcode

--- app.js
+++ app.js
@@ -7,10 +7,8 @@ const app = express();
 
 app.use(morgan('dev'), volleyball, express.static('public'));
 
-
-
 app.get("/", (req, res) => {
-  const posts = postBank.list()
+  const posts = postBank.list();
   const html = `
 <!DOCTYPE html>
   <html>
@@ -37,61 +35,59 @@ app.get("/", (req, res) => {
   </body>
 </html>`
 
-  res.send(html)
+  res.send(html);
 });
 
 app.get('/posts/:id', (req, res) => {
   const id = req.params.id;
   const post = postBank.find(id);
-  
-  if(!post.id){
+
+  if (!post.id) {
     throw new Error("Not Found")
-  }else{
-  const html = `
-   <html>
-  <head>
-    <title>Wizard News</title>
-    <link rel="stylesheet" href="/style.css" />
-  </head>
-  <body>
-  <div class="news-list">
-  <header><img src="/logo.png"/>Wizard News</header>
-  <div class='news-item'>
-  <p>
-   
-    ${post.title}
-    <small>(by ${post.name})</small>
-    </p>
-    <p>${post.content}</p>
-    </div>
-    </body>
-    </html>
-  `
-  res.send(html)
-  }
-
-
-})
+  } else {
+    const html = `
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Wizard News</title>
+        <link rel="stylesheet" href="/style.css" />
+      </head>
+      <body>
+        <div class="news-list">
+          <header><img src="/logo.png"/>Wizard News</header>
+          <div class='news-item'>
+            <p>
+              ${post.title}
+              <small>(by ${post.name})</small>
+            </p>
+            <p>${post.content}</p>
+        </div>
+      </body>
+      </html>
+    `
+    res.send(html)
+  };
+});
 
 app.use((err, req, res, next) => {
   console.error(err.stack)
   const html = ` 
+  <!DOCTYPE html>
   <html>
-  <head>
-    <title>Wizard News</title>
-    <link rel="stylesheet" href="/style.css" />
-  </head>
-  <body>
-  <header><img src="/logo.png"/>Wizard News</header>
-  <h2>Wrong Path I Will Eat Your Face NOMNONLOM 404</h2>
-  </body>
+    <head>
+      <title>Wizard News</title>
+      <link rel="stylesheet" href="/style.css" />
+    </head>
+    <body>
+      <header><img src="/logo.png"/>Wizard News</header>
+      <h2>Wrong Path I Will Eat Your Face NOMNONLOM 404</h2>
+    </body>
   </html>`
+
   res.status(404).send(html)
-})
+});
 
 const PORT = 1337;
-
-
 
 app.listen(PORT, () => {
   console.log("hey hey");
